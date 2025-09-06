bf5bd5b8a282cef9c59b4d23a0a72063b559c898
erichuang0324 <121286342+erichuang0324@users.noreply.github.com>
2025-09-06 14:46:55 +0800

    first commit

---
+++ src/main/java/frc/robot/Commands/TeleDCommand.java
@@ -0,0 +1,25 @@
+package frc.robot.Commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Subsystem.DriveTrainSubsystem;
+
+public class TeleDCommand extends Command{
+    DriveTrainSubsystem subsystem = new DriveTrainSubsystem();
+    CommandXboxController controller = new CommandXboxController(0);
+    public TeleDCommand(DriveTrainSubsystem subsystem, CommandXboxController controller){
+        this.subsystem = subsystem;
+        this.controller = controller;
+    }
+    @Override
+    public void initialize() {
+    }
+    @Override
+    public void execute() {
+        subsystem.drive(-controller.getLeftY(), -controller.getRightY());
+    }
+    @Override
+    public void end(boolean interrupted) {
+        
+    }
+    
+}
---
+++ src/main/java/frc/robot/Subsystem/DriveTrainSubsystem.java
@@ -0,0 +1,42 @@
+package frc.robot.Subsystem;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix6.hardware.CANcoder;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrainSubsystem extends SubsystemBase{
+
+    WPI_TalonSRX frontRight = new WPI_TalonSRX(1);
+    WPI_TalonSRX frontLeft = new WPI_TalonSRX(2);
+    WPI_VictorSPX backLeft = new WPI_VictorSPX(3);
+    WPI_VictorSPX backRight = new WPI_VictorSPX(4);
+
+    CANcoder rightCan = new CANcoder(12);
+    CANcoder leftCan = new CANcoder(11);
+    
+    DifferentialDrive robotDrive = new DifferentialDrive(frontLeft, frontRight);
+    
+
+    public DriveTrainSubsystem(){
+        backLeft.setInverted(true);
+        frontLeft.setInverted(true);
+        
+        frontRight.setInverted(false);
+        backRight.setInverted(false);
+
+        backLeft.follow(frontLeft);
+        backRight.follow(frontRight);
+    }
+    public void drive(double leftSpeed, double rightSpeed){
+        robotDrive.tankDrive(leftSpeed, rightSpeed);
+    }
+    @Override
+    public void periodic() {
+        // TODO Auto-generated method stub
+        super.periodic();
+    }
+
+}
